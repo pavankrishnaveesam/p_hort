51578b2c4d91b9880126a40387f88ce22e18c13b
Veesam Pavan Krishna <pavankrishna2321@gmail.com>
2024-08-03 18:19:38 +0530

    coments

--- assignments-master/week-1/offline-class-1/level-2/01-Class.js
+++ assignments-master/week-1/offline-class-1/level-2/01-Class.js
@@ -1,12 +1,11 @@
-
 class Animal {
   constructor(name, legCount) {
-    this.name = name
-    this.legCount = legCount
+    this.name = name;
+    this.legCount = legCount;
   }
   describe() {
-    return `${this.name} has ${this.legCount} legs`
+    return `${this.name} has ${this.legCount} legs`;
   }
 }
 
-
+console.log(new Animal("cat", 4).describe()); //cat has 4 legs
